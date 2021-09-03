8883bf8fcac063ed6f717e4102d57ab76ff9f95d
권오우 <kwonow1@naver.com>
2021-09-03 11:30:29 +0900

    STYLE : 메인 페이지 css 및 사진 등록
    Former-commit-id: dfec5b8253e2b02c72e7c03aa30b9668c8e7ab9b

--- front/src/routers/Home.js
+++ front/src/routers/Home.js
@@ -1,7 +1,22 @@
+import { Link } from 'react-router-dom'
+import './Home.css'
+
 function Home() {
+    // KOW - main 사진 번호 랜덤 생성 시작
+    let page_num = Math.floor(Math.random() * 8 + 1)
+    if ( page_num === 9) {
+        page_num = 8
+    }
+    // KOW - main 사진 번호 랜덤 생성 끝
+
     return (
-        <div>
-            Home
+        <div className="home">
+            <img src={`/images/main_${page_num}.jpg`} alt={`main_${page_num}`} />
+            <div className="home_bottom">
+                <div className="home_btn">
+                    <Link to="/teller">나만의 향수 추천</Link>
+                </div>
+            </div>
         </div>
     )
 }
