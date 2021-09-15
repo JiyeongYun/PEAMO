c3f4eb519d37b7ee01ee140d22e932cafc3539fc
권오우 <kwonow1@naver.com>
2021-09-15 15:05:54 +0900

    FEAT : 메인 화면 밑에 이달의 향수 추가
    Former-commit-id: 988339051d8c9b63c5fb4122986782c191bed476

--- frontend/src/routers/Home.js
+++ frontend/src/routers/Home.js
@@ -5,11 +5,43 @@ function Home({page_num}) {
 
   return (
     <div className="home">
+      <div className="white_canvas"></div>
       <div className="grey_canvas"></div>
       <img src={`/images/main_${page_num}.jpg`} alt={`main_${page_num}`} />
       <div className="home_bottom">
         <div className="home_btn">
           <Link to="/teller">나만의 향수 추천</Link>
+        </div>
+      </div>
+      <div className="this_month_perfume">
+        <div className="center_pos">
+          <div className="title">이달의 향수</div>
+          <div className="perfumes">
+            <div className="perfume">
+              <img src="/images/lovely.png" alt="perfume" />
+              <div className="perfume_info">
+                <p>Libre</p>
+                <p>Yves Saint Laurent</p>
+              </div>
+              <div>#Lavender #Mandarin Orange #Jasmine #Musk</div>
+            </div>
+            <div className="perfume">
+              <img src="/images/lovely.png" alt="perfume" />
+              <div className="perfume_info">
+                <p>Libre</p>
+                <p>Yves Saint Laurent</p>
+              </div>
+              <div>#Lavender #Mandarin Orange #Jasmine #Musk</div>
+            </div>
+            <div className="perfume">
+              <img src="/images/lovely.png" alt="perfume" />
+              <div className="perfume_info">
+                <p>Libre</p>
+                <p>Yves Saint Laurent</p>
+              </div>
+              <div>#Lavender #Mandarin Orange #Jasmine #Musk</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
