10f22039f01508deff1b21eea0051b679eb6140e
Jang Hyeon Woong <ds6tvi@gmail.com>
2021-09-23 17:59:32 +0900

    FEAT : note 관련 model 추가
    Former-commit-id: 6d7fc48795ec99b17fa78e6503a4a238fb23d887

---
+++ backend/spring/src/main/java/com/osds/peamo/model/entity/Category.java
@@ -0,0 +1,24 @@
+package com.osds.peamo.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="api_category")
+@Builder
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;                         // id
+
+    private String eng;
+}
