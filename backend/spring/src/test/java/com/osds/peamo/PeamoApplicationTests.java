46897b74a97dddaafc7460c33b311f29021edf73
Jang Hyeon Woong <ds6tvi@gmail.com>
2021-09-23 13:50:38 +0900

    FEAT : backend 세팅
    Former-commit-id: 43032f0d374ff4bf579bae085fa35686f6c0a691

---
+++ backend/spring/src/test/java/com/osds/peamo/PeamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osds.peamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeamoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
