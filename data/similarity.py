2916731e184c3bf75840e9e22e23760c4c8871f0
Jang Hyeon Woong <ds6tvi@gmail.com>
2021-09-28 11:24:05 +0900

    FEAT : Datefrme을 MySQL로 넣는 작업
    Former-commit-id: 550c5ad011392329d01960ff95ce3a22dd37b95d

--- data/similarity.py
+++ data/similarity.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from sqlalchemy import *
 from sklearn.metrics.pairwise import cosine_similarity
+from rds_engine import create_engine
 
 df = pd.read_csv('perfume_notes.csv', index_col=0)
 df = df.fillna(0)
@@ -7,3 +9,26 @@ df = df.fillna(0)
 similarity = cosine_similarity(df)
 
 n_df = pd.DataFrame(similarity, columns=df.index, index=df.index)
+
+idx_list = n_df.index
+value = []
+for r in idx_list:
+    for c in idx_list:
+        if r >= c:
+            continue
+        temp = [r, c, n_df.loc[r, c]]
+        value.append(temp)
+    print(r)
+
+s_df = pd.DataFrame(value, columns=['index', 'columns', 'similarity'])
+
+
+
+# 데이터프레임 통째로 MySQL로 넣기
+engine = create_engine()
+conn = engine.connect()
+
+
+s_df.to_sql(name="similarity", con=engine, if_exists='append',index=False)
+
+conn.close()
