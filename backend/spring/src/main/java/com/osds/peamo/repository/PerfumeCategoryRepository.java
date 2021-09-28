8b0f5c0fe4a3cda3971b4a7e8f2cab4729e35452
sachieve7 <sachieve7@naver.com>
2021-09-28 16:39:24 +0900

    FEAT: 향수 검색 기능 구현(진행중 중간 커밋)
    Former-commit-id: 063ae88d748fd7a66e364e8aefe7e1157c43cdd6

---
+++ backend/spring/src/main/java/com/osds/peamo/repository/PerfumeCategoryRepository.java
@@ -0,0 +1,10 @@
+package com.osds.peamo.repository;
+
+import com.osds.peamo.model.entity.PerfumeCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PerfumeCategoryRepository extends JpaRepository<PerfumeCategory, Long> {
+    List<PerfumeCategory> getPerfumeCategoriesByCategoryIdIn(List<Long> categoryIdList);
+}
