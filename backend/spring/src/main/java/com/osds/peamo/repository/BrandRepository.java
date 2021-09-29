7d8eefc7e70be77b52736349e2e0f5d3e0347d6c
sachieve7 <sachieve7@naver.com>
2021-09-29 11:29:49 +0900

    FEAT: 향수 전체 리스트 검색 기능 구현
    Former-commit-id: 5e2f1995809afdde1659e3c006717b7220bfedac

--- backend/spring/src/main/java/com/osds/peamo/repository/BrandRepository.java
+++ backend/spring/src/main/java/com/osds/peamo/repository/BrandRepository.java
@@ -1,6 +1,8 @@
 package com.osds.peamo.repository;
 
 import com.osds.peamo.model.entity.Brand;
+import com.osds.peamo.model.entity.Perfume;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
@@ -8,6 +10,6 @@ import java.util.ArrayList;
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 
     ArrayList<Brand> findAll();
+//    Brand getBrandById(long id);
 
-    Brand getBrandById(Long id);
 }
