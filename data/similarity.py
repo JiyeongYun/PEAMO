abaa8765bce002edfebddb878cc7fb0b3d782f86
권오우 <kwonow1@naver.com>
2021-09-30 02:49:07 +0000

    Update similarity.py
    Former-commit-id: fe747dfce4a4ff777a0cc70132661bb986dab4e3

--- data/similarity.py
+++ data/similarity.py
@@ -9,18 +9,29 @@ df = df.fillna(0)
 similarity = cosine_similarity(df)
 
 n_df = pd.DataFrame(similarity, columns=df.index, index=df.index)
-
+n_df = n_df.drop_duplicates()
+value = []
+i = 0
 for std in n_df.index:
+    i += 1
+    print(std)
     sort_df = n_df.loc[std, :].sort_values(ascending=False)
     id_list = sort_df.head(3).index
     value_list = sort_df.head(3).values
-    temp = [[std, id_list[1], value_list[1]], [std, id_list[2], value_list[2]]]
-    s_df = pd.DataFrame(temp, columns=['index', 'columns', 'similarity'])
+    temp = [std, id_list[1], value_list[1]]
+    value.append(temp)
+    temp = [std, id_list[2], value_list[2]]
+    value.append(temp)
+    if i == 1000:
 
-    # 데이터프레임 통째로 MySQL로 넣기
-    engine = create_engine()
-    conn = engine.connect()
+        s_df = pd.DataFrame(value, columns=['standard', 'comparison', 'similarity'])
 
-    s_df.to_sql(name="similarity", con=engine, if_exists='append', index=False)
+        # 데이터프레임 통째로 MySQL로 넣기
+        engine = create_engine()
+        conn = engine.connect()
 
-    conn.close()
+        s_df.to_sql(name="similarity", con=engine, if_exists='append', index=False)
+
+        conn.close()
+        value = []
+        i = 0
