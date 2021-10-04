f451a9ef73edd8212d915071ece583a8bb7ec64a
권오우 <kwonow1@naver.com>
2021-10-04 22:27:10 +0900

    FEAT : 스무고개 결과 페이지 BE 연결
    Former-commit-id: 8a495573fcd615a1cc278b437be0d2b5db99713d

--- frontend/src/routers/TellerResult.js
+++ frontend/src/routers/TellerResult.js
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './TellerResult.css';
 import { Link } from 'react-router-dom';
+import axios from 'axios'
 
 function Result() {
   // header 검은색으로 변경
@@ -10,46 +11,78 @@ function Result() {
   }, []);
   // header 검은색으로 변경
 
+  // state 값
+  const [perfumeList, setPerfumeList] = useState([]);
+  // axios 요청 보내서 향수 데이터 가져오기
+  if (!perfumeList.length) {
+    const mainCategory = localStorage.getItem('mainCategory');
+    const subCategory = localStorage.getItem('subCategory');
+    const dislikeCategory = localStorage.getItem('dislikeCategory');
+    const season = localStorage.getItem('season');
+    const gender = localStorage.getItem('gender');
+  
+    const data = {
+      mainCategory,
+      subCategory,
+      dislikeCategory,
+      season,
+      gender
+    };
+  
+    axios.post(
+      'http://j5a403.p.ssafy.io:8000/perfume/recommend', data
+    )
+      .then(res => {
+        if (res.status === 200) {
+          setPerfumeList(res.data)
+        };
+      })
+      .catch(err => console.log(err))
+  }
+  // axios 요청 보내서 향수 데이터 가져오기 끝
+
+  console.log(perfumeList.length)
   return (
-    <div className="tellerresult">
-      <div className="teller_result">
-        <div className="result_question">
-          <h2>당신에게 맞는 향수를 골라봤어요!</h2>
-        </div>
-        <div className="result_image">
-          <div className="image2">
-            <Link to="/teller-result">
-              <img src="/images/perfume_byredo.jpeg" alt="byredo"></img>
-            </Link>
-            <Link to="/teller-result">
-              <p>Byredo</p>
-              <p>Bal d'Afrique</p>
-            </Link>
+    perfumeList.length !== 0 &&
+      <div className="tellerresult">
+        <div className="teller_result">
+          <div className="result_question">
+            <h2>당신에게 맞는 향수를 골라봤어요!</h2>
           </div>
-          <div className="image1">
-            <Link to="/teller-result">
-              <img src="/images/perfume_jomalone.jpeg" alt="chloe"></img>
-            </Link>
-            <Link to="/teller-result">
-              <p>Chloe</p>
-              <p>Chloe(new)</p>
-            </Link>
+          <div className="result_image">
+            <div className="image2">
+              <Link to="/teller-result">
+                <img src={perfumeList[1].imgurl} alt={perfumeList[1].name}></img>
+              </Link>
+              <Link to="/teller-result">
+                <p>{perfumeList[1].brand}</p>
+                <p>{perfumeList[1].name}</p>
+              </Link>
+            </div>
+            <div className="image1">
+              <Link to="/teller-result">
+                <img src={perfumeList[0].imgurl} alt={perfumeList[0].name}></img>
+              </Link>
+              <Link to="/teller-result">
+                <p>{perfumeList[0].brand}</p>
+                <p>{perfumeList[0].name}</p>
+              </Link>
+            </div>
+            <div className="image3">
+              <Link to="/teller-result">
+                <img src={perfumeList[2].imgurl} alt={perfumeList[2].name}></img>
+              </Link>
+              <Link to="/teller-result">
+                <p>{perfumeList[2].brand}</p>
+                <p>{perfumeList[2].name}</p>
+              </Link>
+            </div>
           </div>
-          <div className="image3">
-            <Link to="/teller-result">
-              <img src="/images/perfume_hermes.jpeg" alt="giorgio"></img>
-            </Link>
-            <Link to="/teller-result">
-              <p>Hermes</p>
-              <p>Un Jardin en Mediterranee</p>
-            </Link>
+          <div className="result_123">
+            <img src="/images/result_123.png" alt="result_123"></img>
           </div>
-        </div>
-        <div className="result_123">
-          <img src="/images/result_123.png" alt="result_123"></img>
         </div>
       </div>
-    </div>
   );
 }
 
