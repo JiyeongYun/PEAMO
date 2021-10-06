86791c7b6c38081e4d246d47dc51f89b6b2e22cb
sangmin song <ssmin940606@gmail.com>
2021-10-06 13:25:33 +0900

    FEAT: teller result 페이지에서 향수 디테일 모달 연결
    Former-commit-id: b3733684bd561360c5335345ea13bacbc5251bdd

--- frontend/src/routers/TellerResult.js
+++ frontend/src/routers/TellerResult.js
@@ -1,9 +1,22 @@
 import { useEffect, useState } from 'react';
-import './TellerResult.css';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import axios from 'axios'
+import axios from 'axios';
+import './TellerResult.css';
+
+// component
+import PerfumeDetail from '../components/Common/PerfumeDetail';
+
+// action
+import { getPerfumeDetail } from '../components/Common/commonSlice';
 
 function Result() {
+  // state 값
+  const [perfumeList, setPerfumeList] = useState([]);
+  // 향수 디테일 모달 state
+  const [showPerfumeDetail, setShowPerfumeDetail] = useState(false);
+  // useDispatch
+  const dispatch = useDispatch();
   // header 검은색으로 변경
   useEffect(() => {
     const header = document.querySelector('header');
@@ -11,8 +24,6 @@ function Result() {
   }, []);
   // header 검은색으로 변경
 
-  // state 값
-  const [perfumeList, setPerfumeList] = useState([]);
   // axios 요청 보내서 향수 데이터 가져오기
   if (!perfumeList.length) {
     const mainCategory = localStorage.getItem('mainCategory');
@@ -20,43 +31,72 @@ function Result() {
     const dislikeCategory = localStorage.getItem('dislikeCategory');
     const season = localStorage.getItem('season');
     const gender = localStorage.getItem('gender');
-  
+
     const data = {
       mainCategory,
       subCategory,
       dislikeCategory,
       season,
-      gender
+      gender,
     };
-  
-    axios.post(
-      'http://j5a403.p.ssafy.io:8000/perfume/recommend', data
-    )
-      .then(res => {
+
+    axios
+      .post('http://j5a403.p.ssafy.io:8000/perfume/recommend', data)
+      .then((res) => {
         if (res.status === 200) {
-          setPerfumeList(res.data)
-        };
+          setPerfumeList(res.data);
+        }
       })
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err));
   }
   // axios 요청 보내서 향수 데이터 가져오기 끝
 
+  // 향수 디테일 모달 열고 닫는 함수
+  const togglePerfumeDetail = (perfumeId) => {
+    if (!showPerfumeDetail) {
+      // 향수 디테일 api 통신
+      dispatch(getPerfumeDetail(perfumeId))
+        .unwrap()
+        .then(() => {
+          setShowPerfumeDetail(!showPerfumeDetail);
+        });
+    } else {
+      setShowPerfumeDetail(!showPerfumeDetail);
+    }
+  };
+
   return (
-    perfumeList.length !== 0 &&
-      <div className="tellerresult">
-        <img src="/images/result_back.png" alt="back_ground"/>
-        <div className="teller_result">
-          <div className="result_question">
-            <h2>당신에게 맞는 향수를 골라봤어요!</h2>
-          </div>
-          <div className="result_images">
-            {
-              perfumeList.map(perfume => {
+    <>
+      {showPerfumeDetail && (
+        <PerfumeDetail togglePerfumeDetail={togglePerfumeDetail} />
+      )}
+      {perfumeList.length !== 0 && (
+        <div className="tellerresult">
+          <img src="/images/result_back.png" alt="back_ground" />
+          <div className="teller_result">
+            <div className="result_question">
+              <h2>당신에게 맞는 향수를 골라봤어요!</h2>
+            </div>
+            <div className="result_images">
+              {perfumeList.map((perfume) => {
                 return (
-                  <div className="result_image" key={perfume.id}>
+                  <div
+                    className="result_image"
+                    key={perfume.id}
+                    onClick={() => togglePerfumeDetail(perfume.id)}
+                  >
                     <div className="round">
                       <Link to="/teller-result">
-                        <img src={perfume.imgurl==="http://www.basenotes.net/photos/300noimage.png" || perfume.imgurl===undefined?'/images/no_image.png':perfume.imgurl} alt={perfume.name}></img>
+                        <img
+                          src={
+                            perfume.imgurl ===
+                              'http://www.basenotes.net/photos/300noimage.png' ||
+                            perfume.imgurl === undefined
+                              ? '/images/no_image.png'
+                              : perfume.imgurl
+                          }
+                          alt={perfume.name}
+                        ></img>
                       </Link>
                     </div>
                     <Link to="/teller-result">
@@ -64,12 +104,13 @@ function Result() {
                       <p>{perfume.name}</p>
                     </Link>
                   </div>
-                )
-              })
-            }
+                );
+              })}
+            </div>
           </div>
         </div>
-      </div>
+      )}
+    </>
   );
 }
 
